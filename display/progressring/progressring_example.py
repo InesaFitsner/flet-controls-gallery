d1b83fc6cd535c471989d9611a131727324d36cc
InesaFitsner <inesa@appveyor.com>
2023-02-23 12:37:38 -0800

    progressring example

--- display/progressring/progressring_example.py
+++ display/progressring/progressring_example.py
@@ -1,12 +1,12 @@
 import flet as ft
 
-name = "ProgressBar Example"
+name = "ProgressRing Example"
 
 def example():
     from time import sleep
 
     t = ft.Text(value="Click the button...")
-    pb = ft.ProgressBar(width=400, value=0)
+    pr = ft.ProgressRing(width=16, height=16, stroke_width = 2)
     
 
     def button_clicked(e):
@@ -15,9 +15,9 @@ def example():
         b.disabled = True
         b.update()
         for i in range(0, 101):
-            pb.value = i * 0.01
+            pr.value = i * 0.01
             sleep(0.1)
-            pb.update()
+            pr.update()
         t.value = "Click the button..."
         t.update()
         b.disabled = False
@@ -27,10 +27,13 @@ def example():
     
     return ft.Column(
             [
-                ft.Text("Linear progress indicator", style="headlineSmall"),
-                ft.Column([t, pb]),
-                ft.Text("Indeterminate progress bar", style="headlineSmall"),
-                ft.ProgressBar(width=400, color="amber", bgcolor="#eeeeee"),
+                ft.Text("Circular progress indicator", style="headlineSmall"),
+                ft.Row([pr, t]),
+                ft.Text("Indeterminate cicrular progress", style="headlineSmall"),
+                ft.Column(
+                    [ft.ProgressRing(), ft.Text("I'm going to run for ages...")],
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                    ),
                 b
             ],
             width=400,
