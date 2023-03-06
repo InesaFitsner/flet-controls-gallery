f176b65716a9e662092f689d48931eced0d55111
InesaFitsner <inesa@appveyor.com>
2023-03-06 09:53:07 -0800

    Update navigationbar_example.py

--- navigation/navigationbar/navigationbar_example.py
+++ navigation/navigationbar/navigationbar_example.py
@@ -4,4 +4,14 @@ name = "NavigationBar Example"
 
 def example():
     
-    return ft.Text("This example is under construction")
+    return ft.NavigationBar(
+        destinations=[
+            ft.NavigationDestination(icon=ft.icons.EXPLORE, label="Explore"),
+            ft.NavigationDestination(icon=ft.icons.COMMUTE, label="Commute"),
+            ft.NavigationDestination(
+                icon=ft.icons.BOOKMARK_BORDER,
+                selected_icon=ft.icons.BOOKMARK,
+                label="Explore",
+            ),
+        ]
+    )
