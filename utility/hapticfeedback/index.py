de25e9c4b5db254d95efe696fda11f5a1d9593d0
InesaFitsner <inesa@appveyor.com>
2023-03-10 09:09:18 -0800

    shadermask examples

--- utility/hapticfeedback/index.py
+++
@@ -1,7 +0,0 @@
-name = "HapticFeedback"
-
-description = """Allows access to the haptic feedback interface on the device.
-
-It is non-visual and should be added to `page.overlay` list."""
-
-image_file = "appbar.svg"
