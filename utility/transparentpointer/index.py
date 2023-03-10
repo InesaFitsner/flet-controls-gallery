8ae0510ab432fb7cec2db0819b4ac6f03b732407
InesaFitsner <inesa@appveyor.com>
2023-03-10 09:03:59 -0800

    Delete index.py

--- utility/transparentpointer/index.py
+++
@@ -1,9 +0,0 @@
-name = "TransparentPointer"
-
-description = """TransparentPointer is the solution to "How to pass trough all gestures between two widgets in Stack" (https://stackoverflow.com/questions/65269190/pass-trough-all-gestures-between-two-widgets-in-stack) problem.
-
-For example, if there is an ElevatedButton inside Container with GestureDetector then tapping on a button won't be "visible" to a gesture detector behind it. With TransparentPointer a tapping event doesn't stop on a button, but goes up to the parent, similar to event bubbling in HTML/JS.
-
-"""
-
-image_file = "appbar.svg"
