be39f324fd23d9c5ee51ebfc73d505eaa30c5f1c
InesaFitsner <inesa@appveyor.com>
2023-03-10 09:12:48 -0800

    Delete index.py

--- utility/windowdragarea/index.py
+++
@@ -1,7 +0,0 @@
-name = "WindowDragArea"
-
-description = """A control for drag to move, maximize and restore application window.
-
-When you have hidden the title bar with page.window_title_bar_hidden, you can add this control to move the window position."""
-
-image_file = "appbar.svg"
