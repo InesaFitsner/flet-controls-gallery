1d9dd667c3753e2af8234f7e0082bbb9710c7905
InesaFitsner <inesa@appveyor.com>
2023-03-10 12:31:13 -0800

    Update main.py
    refactor

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ gallery = GalleryData()
 
 def main(page: ft.Page):
     
+    def get_route_list(route):
+        route_list = [item for item in route.split("/") if item !=""]
+        return route_list
+
     def route_change(e):
         print("Route:", page.route)
-        route_list = [item for item in page.route.split("/") if item !=""]
+        route_list = get_route_list(page.route)
         print(route_list)
         if len(route_list) == 0:
-            print("Root")
+            page.go("/layout")
         elif len(route_list) == 1:
             display_control_group(route_list[0])
         elif len(route_list) == 2:
@@ -23,21 +27,7 @@ def main(page: ft.Page):
         "Roboto Mono": "RobotoMono-VariableFont_wght.ttf",
     }
 
-    def show_code(e):
-        dlg.open = True
-        code_example_name.value = e.control.data.name
-        code_markdown = ft.Markdown(
-            e.control.data.source_code,
-            selectable=True,
-            extension_set="gitHubWeb",
-            code_theme="atom-one-dark",
-            code_style=ft.TextStyle(font_family="Roboto Mono"),
-            #on_tap_link=lambda e: ft.page.launch_url(e.data),
-        )
-        #dlg.content = ft.ListView(width=500, controls=[code_markdown])
-        
-        dlg.content = ft.Column(controls=[code_markdown], scroll="always")
-        page.update()   
+  
 
     def find_control_group_object(control_group_name):
         for control_group in gallery.destinations_list:
@@ -51,8 +41,9 @@ def main(page: ft.Page):
                 return grid_item
 
     def display_control_examples(control_group_name, control_id):
+        control_group = find_control_group_object(control_group_name)
+        rail.selected_index=gallery.destinations_list.index(control_group)
         grid_item = find_grid_object(control_group_name, control_id)
-        print(f"Display control examples for {grid_item.name}")
         grid.visible = False
         examples.visible = True
         listview.controls = []
@@ -80,6 +71,7 @@ def main(page: ft.Page):
 
     def display_control_group(control_group_name):
         control_group = find_control_group_object(control_group_name)
+        rail.selected_index=gallery.destinations_list.index(control_group)
         grid.visible = True
         examples.visible = False
         grid.controls = []
@@ -99,52 +91,32 @@ def main(page: ft.Page):
                 ))
         page.update()
 
-    def grid_item_clicked(e):
-        page.go(f"{page.route}/{e.control.data.id}")
-        grid.visible = False
-        examples.visible = True
-        listview.controls = []
-        control_name.value = e.control.data.name
-        control_description.value = e.control.data.description
-        for example in e.control.data.examples:
-            listview.controls.append(ft.Column(controls = [
-            ft.Container(content=ft.Row(alignment=ft.MainAxisAlignment.SPACE_BETWEEN, controls = [
-                ft.Text(example.name, style=ft.TextThemeStyle.TITLE_MEDIUM, weight=ft.FontWeight.W_500), 
-                ft.IconButton(icon=ft.icons.CODE, on_click=show_code, data=example)
-                ]),
-                bgcolor=ft.colors.SECONDARY_CONTAINER,
-                padding=5,
-                border_radius=5
-                ),
-            ft.Row(controls = [
-                ft.Container(content=example.example()), 
-                ]), 
-                ]
-            )
-        )
-                
-        page.update()
-
     def control_group_selected(e):
-        page.go(f"/{gallery.destinations_list[e.control.selected_index].name}")
-        grid.visible = True
-        examples.visible = False
-        grid.controls = []
-        listview.controls = []
-        for grid_item in gallery.destinations_list[e.control.selected_index].grid_items:
-            grid.controls.append(ft.Container(
-                on_click=grid_item_clicked,
-                data=grid_item, 
-                bgcolor=ft.colors.SECONDARY_CONTAINER,
-                content=ft.Column(
-                    alignment=ft.MainAxisAlignment.SPACE_EVENLY,
-                    horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-                    controls=[
-                        ft.Image(src=grid_item.image_file_name, width=40), 
-                        ft.Text(value=grid_item.name, style=ft.TextThemeStyle.TITLE_SMALL)]
-                    )
-                ))
-        page.update()
+        control_group_name = gallery.destinations_list[e.control.selected_index].name
+        page.go(f"/{control_group_name}")
+        display_control_group(control_group_name)
+    
+    def grid_item_clicked(e):
+        route = f"{page.route}/{e.control.data.id}"
+        page.go(route)
+        route_list = get_route_list(route)
+        display_control_examples(route_list[0], route_list[1])
+
+    def show_code(e):
+        dlg.open = True
+        code_example_name.value = e.control.data.name
+        code_markdown = ft.Markdown(
+            e.control.data.source_code,
+            selectable=True,
+            extension_set="gitHubWeb",
+            code_theme="atom-one-dark",
+            code_style=ft.TextStyle(font_family="Roboto Mono"),
+            #on_tap_link=lambda e: ft.page.launch_url(e.data),
+        )
+        #dlg.content = ft.ListView(width=500, controls=[code_markdown])
+        
+        dlg.content = ft.Column(controls=[code_markdown], scroll="always")
+        page.update() 
     
     def get_destinations():
         destinations = []
