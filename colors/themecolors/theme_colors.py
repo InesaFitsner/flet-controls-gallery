c044dffb4f6817bf79f02e54cbe57c7e7c9773b5
InesaFitsner <inesa@appveyor.com>
2023-03-27 11:38:47 -0700

    Update theme_colors.py

--- colors/themecolors/theme_colors.py
+++ colors/themecolors/theme_colors.py
@@ -103,6 +103,133 @@ def example():
                         alignment = ft.alignment.center,
                         border_radius=ft.border_radius.only(bottomLeft=10, bottomRight=10)
                     )
+                ])),
+        ft.Container(
+            border_radius=10,
+            #border=ft.border.all(1, ft.colors.ON_SECONDARY_CONTAINER),
+            #margin=10,
+            #padding=10,
+            content=ft.Column(
+                spacing=0,
+                controls=[
+                    ft.Container(
+                        height=50, 
+                        bgcolor=ft.colors.ERROR, 
+                        content=ft.Text("ERROR"), 
+                        alignment = ft.alignment.center, 
+                        border_radius=ft.border_radius.only(topLeft=10, topRight=10)
+                        ),
+                    ft.Container(
+                        height=50, 
+                        bgcolor=ft.colors.ON_ERROR, 
+                        content=ft.Text("ON_ERROR"), 
+                        alignment = ft.alignment.center),
+                    ft.Container(
+                        height=50, 
+                        bgcolor=ft.colors.ERROR_CONTAINER, 
+                        content=ft.Text("ERROR_CONTAINER"), 
+                        alignment = ft.alignment.center),
+                    ft.Container(
+                        height=50, 
+                        bgcolor=ft.colors.ON_ERROR_CONTAINER, 
+                        content=ft.Text("ON_ERROR_CONTAINER"), 
+                        alignment = ft.alignment.center,
+                        border_radius=ft.border_radius.only(bottomLeft=10, bottomRight=10)
+                    )
+                ])),
+        ft.Container(
+            border_radius=10,
+            #border=ft.border.all(1, ft.colors.ON_SECONDARY_CONTAINER),
+            #margin=10,
+            #padding=10,
+            content=ft.Column(
+                spacing=0,
+                controls=[
+                    ft.Container(
+                        height=50, 
+                        bgcolor=ft.colors.BACKGROUND, 
+                        content=ft.Text("BACKGROUND"), 
+                        alignment = ft.alignment.center, 
+                        border_radius=ft.border_radius.only(topLeft=10, topRight=10)
+                        ),
+                    ft.Container(
+                        height=50, 
+                        bgcolor=ft.colors.ON_BACKGROUND, 
+                        content=ft.Text("ON_BACKGROUND"), 
+                        alignment = ft.alignment.center,
+                        border_radius=ft.border_radius.only(bottomLeft=10, bottomRight=10)
+                    )
+                ])),
+        ft.Container(
+            border_radius=10,
+            #border=ft.border.all(1, ft.colors.ON_SECONDARY_CONTAINER),
+            #margin=10,
+            #padding=10,
+            content=ft.Column(
+                spacing=0,
+                controls=[
+                    ft.Container(
+                        height=50, 
+                        bgcolor=ft.colors.SURFACE, 
+                        content=ft.Text("SURFACE"), 
+                        alignment = ft.alignment.center, 
+                        border_radius=ft.border_radius.only(topLeft=10, topRight=10)
+                        ),
+                    ft.Container(
+                        height=50, 
+                        bgcolor=ft.colors.ON_SURFACE, 
+                        content=ft.Text("ON_SURFACE"), 
+                        alignment = ft.alignment.center),
+                    ft.Container(
+                        height=50, 
+                        bgcolor=ft.colors.SURFACE_VARIANT, 
+                        content=ft.Text("SURFACE_VARIANT"), 
+                        alignment = ft.alignment.center),
+                    ft.Container(
+                        height=50, 
+                        bgcolor=ft.colors.ON_SURFACE_VARIANT, 
+                        content=ft.Text("ON_SURFACE_VARIANT"), 
+                        alignment = ft.alignment.center,
+                        border_radius=ft.border_radius.only(bottomLeft=10, bottomRight=10)
+                    )
+                ])),
+        ft.Container(
+            border_radius=10,
+            #border=ft.border.all(1, ft.colors.ON_SECONDARY_CONTAINER),
+            #margin=10,
+            #padding=10,
+            content=ft.Column(
+                spacing=0,
+                controls=[
+                    ft.Container(
+                        height=50, 
+                        bgcolor=ft.colors.OUTLINE, 
+                        content=ft.Text("OUTLINE"), 
+                        alignment = ft.alignment.center, 
+                        border_radius=ft.border_radius.only(topLeft=10, topRight=10)
+                        ),
+                    ft.Container(
+                        height=50, 
+                        bgcolor=ft.colors.SHADOW, 
+                        content=ft.Text("SHADOW"), 
+                        alignment = ft.alignment.center),
+                    ft.Container(
+                        height=50, 
+                        bgcolor=ft.colors.INVERSE_SURFACE, 
+                        content=ft.Text("INVERSE_SURFACE"), 
+                        alignment = ft.alignment.center),
+                    ft.Container(
+                        height=50, 
+                        bgcolor=ft.colors.ON_INVERSE_SURFACE, 
+                        content=ft.Text("ON_INVERSE_SURFACE"), 
+                        alignment = ft.alignment.center),
+                    ft.Container(
+                        height=50, 
+                        bgcolor=ft.colors.INVERSE_PRIMARY, 
+                        content=ft.Text("INVERSE_PRIMARY"), 
+                        alignment = ft.alignment.center,
+                        border_radius=ft.border_radius.only(bottomLeft=10, bottomRight=10)
+                    )
                 ]))
 
     ])
