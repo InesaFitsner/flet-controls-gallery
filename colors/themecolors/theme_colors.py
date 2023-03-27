8709dac0dcf98e560737d8db65a945e6ad57c845
InesaFitsner <inesa@appveyor.com>
2023-03-27 11:26:12 -0700

    Update theme_colors.py

--- colors/themecolors/theme_colors.py
+++ colors/themecolors/theme_colors.py
@@ -5,13 +5,71 @@ name = "Theme colors"
 def example():
 
     return ft.ListView(width=500, spacing=10, padding=20, auto_scroll=True, controls=[
-        ft.Container(height=50, bgcolor=ft.colors.PRIMARY, content=ft.Text("PRIMARY"), alignment = ft.alignment.center),
-        ft.Container(height=50, bgcolor=ft.colors.ON_PRIMARY, content=ft.Text("ON_PRIMARY"), alignment = ft.alignment.center),
-        ft.Container(height=50, bgcolor=ft.colors.PRIMARY_CONTAINER, content=ft.Text("PRIMARY_CONTAINER"), alignment = ft.alignment.center),
-        ft.Container(height=50, bgcolor=ft.colors.ON_PRIMARY_CONTAINER, content=ft.Text("ON_PRIMARY_CONTAINER"), alignment = ft.alignment.center),
-        ft.Container(height=50, bgcolor=ft.colors.SECONDARY, content=ft.Text("SECONDARY"), alignment = ft.alignment.center),
-        ft.Container(height=50, bgcolor=ft.colors.ON_SECONDARY, content=ft.Text("ON_SECONDARY"), alignment = ft.alignment.center),
-        ft.Container(height=50, bgcolor=ft.colors.SECONDARY_CONTAINER, content=ft.Text("SECONDARY_CONTAINER"), alignment = ft.alignment.center),
-        ft.Container(height=50, bgcolor=ft.colors.ON_SECONDARY_CONTAINER, content=ft.Text("ON_SECONDARY_CONTAINER"), alignment = ft.alignment.center),
+        ft.Container(
+            border_radius=10,
+            #border=ft.border.all(1, ft.colors.ON_SECONDARY_CONTAINER),
+            #margin=10,
+            #padding=10,
+            content=ft.Column(
+                spacing=0,
+                controls=[
+                    ft.Container(
+                        height=50, 
+                        bgcolor=ft.colors.PRIMARY, 
+                        content=ft.Text("PRIMARY"), 
+                        alignment = ft.alignment.center, 
+                        border_radius=ft.border_radius.only(topLeft=10, topRight=10)
+                        ),
+                    ft.Container(
+                        height=50, 
+                        bgcolor=ft.colors.ON_PRIMARY, 
+                        content=ft.Text("ON_PRIMARY"), 
+                        alignment = ft.alignment.center),
+                    ft.Container(
+                        height=50, 
+                        bgcolor=ft.colors.PRIMARY_CONTAINER, 
+                        content=ft.Text("PRIMARY_CONTAINER"), 
+                        alignment = ft.alignment.center),
+                    ft.Container(
+                        height=50, 
+                        bgcolor=ft.colors.ON_PRIMARY_CONTAINER, 
+                        content=ft.Text("ON_PRIMARY_CONTAINER"), 
+                        alignment = ft.alignment.center,
+                        border_radius=ft.border_radius.only(bottomLeft=10, bottomRight=10)
+                    )
+                ])),
+        ft.Container(
+            border_radius=10,
+            #border=ft.border.all(1, ft.colors.ON_SECONDARY_CONTAINER),
+            #margin=10,
+            #padding=10,
+            content=ft.Column(
+                spacing=0,
+                controls=[
+                    ft.Container(
+                        height=50, 
+                        bgcolor=ft.colors.SECONDARY, 
+                        content=ft.Text("SECONDARY"), 
+                        alignment = ft.alignment.center, 
+                        border_radius=ft.border_radius.only(topLeft=10, topRight=10)
+                        ),
+                    ft.Container(
+                        height=50, 
+                        bgcolor=ft.colors.ON_SECONDARY, 
+                        content=ft.Text("ON_SECONDARY"), 
+                        alignment = ft.alignment.center),
+                    ft.Container(
+                        height=50, 
+                        bgcolor=ft.colors.SECONDARY_CONTAINER, 
+                        content=ft.Text("SECONDARY_CONTAINER"), 
+                        alignment = ft.alignment.center),
+                    ft.Container(
+                        height=50, 
+                        bgcolor=ft.colors.ON_SECONDARY_CONTAINER, 
+                        content=ft.Text("ON_SECONDARY_CONTAINER"), 
+                        alignment = ft.alignment.center,
+                        border_radius=ft.border_radius.only(bottomLeft=10, bottomRight=10)
+                    )
+                ])),
 
     ])
