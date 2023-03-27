64a726aa37c728f6c2ec67056c55c26467ff561d
InesaFitsner <inesa@appveyor.com>
2023-03-27 11:28:36 -0700

    Update theme_colors.py

--- colors/themecolors/theme_colors.py
+++ colors/themecolors/theme_colors.py
@@ -71,5 +71,38 @@ def example():
                         border_radius=ft.border_radius.only(bottomLeft=10, bottomRight=10)
                     )
                 ])),
+        ft.Container(
+            border_radius=10,
+            #border=ft.border.all(1, ft.colors.ON_SECONDARY_CONTAINER),
+            #margin=10,
+            #padding=10,
+            content=ft.Column(
+                spacing=0,
+                controls=[
+                    ft.Container(
+                        height=50, 
+                        bgcolor=ft.colors.TERTIARY, 
+                        content=ft.Text("TERTIARY"), 
+                        alignment = ft.alignment.center, 
+                        border_radius=ft.border_radius.only(topLeft=10, topRight=10)
+                        ),
+                    ft.Container(
+                        height=50, 
+                        bgcolor=ft.colors.ON_TERTIARY, 
+                        content=ft.Text("ON_TERTIARY"), 
+                        alignment = ft.alignment.center),
+                    ft.Container(
+                        height=50, 
+                        bgcolor=ft.colors.TERTIARY_CONTAINER, 
+                        content=ft.Text("TERTIARY_CONTAINER"), 
+                        alignment = ft.alignment.center),
+                    ft.Container(
+                        height=50, 
+                        bgcolor=ft.colors.ON_TERTIARY_CONTAINER, 
+                        content=ft.Text("ON_TERTIARY_CONTAINER"), 
+                        alignment = ft.alignment.center,
+                        border_radius=ft.border_radius.only(bottomLeft=10, bottomRight=10)
+                    )
+                ]))
 
     ])
